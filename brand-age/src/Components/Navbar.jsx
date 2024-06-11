533e3cb067745e996a27a11bb857ca49d3863e3d
L3vi Ackerman <l3vi3211@gmail.com>
2024-06-11 14:19:19 +0545

    Responsive css for Navbar links

--- brand-age/src/Components/Navbar.jsx
+++ brand-age/src/Components/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import Button from "./Button";
+import { useState } from "react";
 import '../App.css'
+import { click } from "@testing-library/user-event/dist/click";
 function Navbar() {
+    const [clickState, setClickState] = useState(false);
+
+    const handleClick = () => {
+        setClickState(prevState => !prevState)
+    }
+
+
     return (
         <>
             <div className="navbar">
                 <div className="navbar-logo">
                     <img src="/logo.png" alt="Nike logo" height={50} width={100} />
                 </div>
-                <div className="navbar-links">
+                <div className={clickState ? 'navbar-links active' :'navbar-links'} >
                     <li><a href="">MENU</a></li>
                     <li><a href="">LOCATION</a></li>
                     <li><a href="">ABOUT</a></li>
                     <li><a href="">CONTACT</a></li>
-                    <i class="ri-menu-3-line"></i>
+                    
                 </div>
+                <div className="mobile" onClick={handleClick}>
+                        <i id='bar' className={clickState ?'ri-close-fill' : 'ri-menu-3-line'}></i>
+                    </div>
                 <div className="navbar-login-btn">
                     <Button className='login-btn' title='Login' />
                 </div>
